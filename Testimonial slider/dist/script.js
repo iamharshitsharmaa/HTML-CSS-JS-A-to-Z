b7dd7c5cde4432dc502cf7d0f42d4f071ec13e98
Harshit Sharma <harshitsharma7222@gmail.com>
2025-01-28 19:06:52 +0530

    Project Updated

---
+++ Testimonial slider/dist/script.js
@@ -0,0 +1,25 @@
+let data = [
+  {
+    name: "Harshal",        
+    description: "Innovative ideas inspiring progress and creative solutions.",
+    image: "",
+  },
+  {
+    name: "Sarthak",
+    description: "Transforming visions into impactful results with passion.",
+    image: "",
+  },
+  {
+    name: "Kinky",
+    description: "Empowering growth through collaboration and innovative thinking.",
+    image: "",
+  },
+];
+
+let name = document.getElementById("name");
+let image = document.getElementById("image");
+let bio = document.getElementById("bio");
+let index = 0;
+let slider = ()=>{
+   
+}
